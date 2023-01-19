220a17e785118c59d7dfec7cce053a203eb1af28
Yasin <yasinworkahad8@gmail.com>
2023-01-19 15:25:19 +0200

    Add project files.

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; } //primary key
+
+        [Column(TypeName = "nvarchar(50)")] //add Column attribute to specify the corresponding SQL Server data type
+        [Required(ErrorMessage = "Title is required.")] //validation 
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon //show the icon with the title next to each other in the Index.cshtml view
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
+}
+
